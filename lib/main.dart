4f7eca52bc9b69649d95277f0b3af5473ef740ad
abhishek prajapati <abhishekprajapati.890e@gmail.com>
2023-03-18 10:33:10 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  Widget build( BuildContext context){
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/abhishek.jpeg'),
+              ),
+              Text(
+                'Abhishek',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans pro',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                   leading :  Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+
+                    title : Text(
+                        '+91 123 456 789',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans pro',
+                      ),
+                    ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                    leading : Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+
+                title: Text(
+                        'abhishek.rediffmail.com',
+                      style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans pro',
+                    ),
+                    ),
+                ),
+              ),
+            ],
+          ),
+          ),
+        ),
+      );
+  }
 }
